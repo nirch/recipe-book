81b75b700bb4d5812e74ee3a6cc5d80f58ec9094
Your Name <you@example.com>
2017-07-26 10:37:36 +0300

    Model user tested

--- app/app.js
+++ app/app.js
@@ -1,1 +1,21 @@
 var recipeApp = angular.module("RecipeApp", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
+
+recipeApp.controller("TestCtrl", function($scope, User, activeUser) {
+    var plainUser = {
+         "email":"nir@nir.com",
+        "password": "nir123",
+        "firstName":"Nir",
+        "lastName": "Channes",
+        "data":"nir-recipes.json"
+    }
+
+    var user = new User(plainUser);
+
+    console.log(JSON.stringify(user));
+    console.log(activeUser.isLoggedIn());
+    activeUser.login(user);
+    console.log(JSON.stringify(activeUser.get()));
+    console.log(activeUser.isLoggedIn());
+    activeUser.logout();
+    console.log(activeUser.isLoggedIn());
+});
