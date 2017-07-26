c4605abeb2f50bf6b10e626c592ae0011b6c75af
Your Name <you@example.com>
2017-07-26 12:18:41 +0300

    Login logic

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,7 +1,42 @@
-recipeApp.controller("LoginCtrl", function ($scope, $uibModalInstance) {
-    $scope.test = "Nir";
+recipeApp.controller("LoginCtrl", function ($scope, $uibModalInstance, $http, $location, activeUser, User) {
 
-    $scope.dismiss = function() {
+    $http.get("app/data/users.json").then(function (response) {
+        $scope.users = [];
+        for (var i = 0; i < response.data.length; i++) {
+            $scope.users.push(new User(response.data[i]));
+        }
+
+        //console.log(JSON.stringify($scope.users));
+    });
+
+    $scope.failedAttempt = false;
+
+    $scope.login = function() {
+        var user = getLoggedInUser();
+        if (user != null) {
+            activeUser.login(user);
+            $uibModalInstance.close("Logged-in");
+            $location.path("/recipes")
+        } else {
+            $scope.failedAttempt = true;
+        }
+
+
+
+
+        //console.log($scope.email + " " + $scope.password)
+    }
+
+    var getLoggedInUser = function() {
+        for (var i = 0; i < $scope.users.length; i++) {
+            if ($scope.users[i].email === $scope.email && $scope.users[i].password === $scope.password) {
+                return $scope.users[i];
+            }
+        }
+        return null;
+    }
+
+    $scope.dismiss = function () {
         $uibModalInstance.close("User dismissed");
     }
 });
